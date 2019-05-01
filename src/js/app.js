4a5cbd54408ae19f571fe487df441574c10a3b55
Medha08 <medha8pandey@gmail.com>
2019-05-01 12:23:10 +0530

    Integrate UI

--- src/js/app.js
+++ src/js/app.js
@@ -45,6 +45,17 @@ App = {
 
   initContract: function() {
    
+    $.getJSON("Adoption.json",function(data){
+
+      var AdoptionArtifact = data;
+      //creates an instance of a contract to interact with
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+      return App.markAdopted();
+
+    })
 
     return App.bindEvents();
   },
@@ -54,9 +65,21 @@ App = {
   },
 
   markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+
+    var adoptionInstance;
+    App.contract.Adoption.deployed().then(function(instance){
+      adoptionInstance = instance;
+
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adotpters){
+      for(i=0;i<adopters.length;i++){
+        if(adopters[i] !== '0x0000000000000000000000000000000000000000' ){
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function(err){
+      console.log(err.message);
+    })
   },
 
   handleAdopt: function(event) {
@@ -64,9 +87,28 @@ App = {
 
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function(err,accounts){
+      if(error){
+        console.log(error);
+      }
+
+      var account = accounts[0];
+      App.contract.Adoption.deployed().then(function(instance){
+        
+        adoptionInstance = instance;
+
+        return adoptionInstance.adoptPet(petId,{from:account}).then(function(result){
+          return App.markAdopted();
+        }).catch(function(err){
+          console.log(err.message);
+        })
+  
+      });
+
+    });
+   
   }
 
 };
